cac7f336dee4ffe987206660fadbbbcd1ed6281f
DiamondYuanqi <60084320+DiamondYuanqi@users.noreply.github.com>
2020-11-25 08:43:32 +0800

    Add files via upload

---
+++ recursion/TowerOfHanoi.cpp
@@ -0,0 +1,16 @@
+//
+// Created by 袁琦 on 2020/11/25.
+//
+
+//A ->C , 先将n-1个盘子移动到B，将第n个移动到C 后将n-1个盘子移动到C
+
+#include<iostream>
+
+using
+
+
+int main(){
+    int n;
+    cin>>n;
+    Hanoi(A,B,C);
+}
