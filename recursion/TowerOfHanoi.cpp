dbb3893b503dc8c5bb0102b8e3d49d867d938077
DiamondYuanqi <60084320+DiamondYuanqi@users.noreply.github.com>
2020-11-26 07:48:52 +0800

    Update TowerOfHanoi.cpp

--- recursion/TowerOfHanoi.cpp
+++ recursion/TowerOfHanoi.cpp
@@ -6,11 +6,24 @@
 
 #include<iostream>
 
-using
+using namespace std;
+
+void Hanoi(int n,char src,char mid ,char dest){
+    if(n==1){//只需移动一个盘子
+        cout<<src<<"->"<<dest<<endl;//直接将盘子从src移动到dest即可
+        return;//递归终止
+    }
+    Hanoi(n-1,src,dest,mid);//先将n-1个盘子从src移动到mid
+    cout<<src<<"->"<<dest<<endl;//再将一个盘子从src移动到dest
+    Hanoi(n-1,mid,src,dest);//最后将n-1个盘子从mid移动到dest
+    return ;
+}
 
 
 int main(){
     int n;
     cin>>n;
-    Hanoi(A,B,C);
+   
+    Hanoi(n,'A','B','C');
+    return 0;
 }
